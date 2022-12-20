7ff4d49b6184e806de3ea91ccc9fe08cdd6fd175
Simon <simon.schleich@univie.ac.at>
2022-12-20 15:32:23 +0100

    MultiFileHandling + Plotting

--- MODULES/PLOTTING/plotting_general.py
+++ MODULES/PLOTTING/plotting_general.py
@@ -1,3 +1,48 @@
-"""
-Settings for plotting. rcParams setting should go here as well
-"""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import MODULES.DATA.data_readin as dar
+
+
+def rc_setup():
+    """Generalized plot attributes"""
+    mpl.rcParams["xtick.direction"] = "in"
+    mpl.rcParams["xtick.labelsize"] = "large"
+    mpl.rcParams["xtick.major.width"] = 1.5
+    mpl.rcParams["xtick.minor.width"] = 1.5
+    mpl.rcParams["xtick.minor.visible"] = "True"
+    mpl.rcParams["xtick.top"] = "True"
+
+    mpl.rcParams["ytick.direction"] = "in"
+    mpl.rcParams["ytick.labelsize"] = "large"
+    mpl.rcParams["ytick.major.width"] = 1.5
+    mpl.rcParams["ytick.minor.width"] = 1.5
+    mpl.rcParams["ytick.minor.visible"] = "True"
+    mpl.rcParams["ytick.right"] = "True"
+
+    mpl.rcParams["axes.grid"] = "False"
+    mpl.rcParams["axes.linewidth"] = 1.5
+    mpl.rcParams["axes.labelsize"] = "large"
+
+    mpl.rcParams["legend.frameon"] = "False"
+
+
+# TODO: Check type hinting for this (specifically "ax")
+def retr_spec(data: dar.TaurexSolution, ax):
+    """DOC!"""
+    # Paramters for ease of reference
+    wavel = data.bin_solution.wlgrid
+    spec = data.bin_solution.spectrum
+    stddev = data.bin_solution.std
+
+    ax.plot(wavel, spec, label=data.name_id)
+
+    # TODO: SHADED AREA WORKS, BUT STDDEV IS VERY SMALL
+    ax.fill_between(
+        x=wavel, y1=spec - stddev, y2=spec + stddev,
+        alpha=0.5
+    )
+
+
+def obs_spec(data: dar.TaurexSolution, ax):
+    """DOC!"""
+    pass
--- main.py
+++ main.py
@@ -1,87 +1,36 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import h5py as h5
-from MODULES.DATA import data_readin as dar
-"""
-GOAL:   Reproduce (somewhat) the plots provided by TauREx, but make them
-        "nicer" (frame spacing etc.)
-        
-        The "Solutions" substructure stores numbered solutions. I wonder
-        if this implies that several runs can be chained and evaluated
-        together (maybe ask Ingo Waldmann about this)
 
-PARTS:  Read in the data product. This will be a hdf5-file of the 
-        retrieval results, with all necessary parameters
-        
-        Create Class/Dictionary storing all relevant information
-        
-        Create plotting routines, maybe they could read in a parameter
-        file to determine what aspects to plot?
+import MODULES.DATA.data_readin as dar
+import MODULES.PLOTTING.plotting_general as pg
 
-
-PSEUDCODE
-
-"""
-
+# GLOBALS
 PLOT_SAVE_DIR = "PLOTS"
-FILE = "DATA/retrieval.hdf5"
-file_raw = h5.File(FILE)
-
-output = file_raw["Output"]
-sol0 = output["Solutions"]["solution0"]
-
-test_ret = dar.TaurexSolution(identifier="Test", solution_group=sol0)
-
-test1 = sol0["Spectra"]["Contributions"]["Absorption"]["binned_spectrum"][()]
-
-spectrum = sol0["Spectra"]
-
-binned_ret = spectrum["binned_spectrum"][()]
-binned_std = spectrum["binned_std"][()]
-binned_wl = spectrum["binned_wlgrid"][()]
-binned_wlwidth = spectrum["binned_wlwidth"][()]
-
-plt.plot(binned_wl, binned_ret)
-plt.plot(binned_wl, test1)
-plt.show()
-exit()
-
-plt.plot(binned_wl, binned_ret, marker="o")
-plt.fill_between(binned_wl, y1=binned_ret-binned_std,
-                 y2=binned_ret + binned_std, alpha=0.5)
-plt.show()
-
-exit()
-# Binned spectrum
-binned_spectrum = spectrum["binned_wlgrid"][()]
-absorp = sol0["Spectra"]["Contributions"]["Absorption"]
-co_xsec = absorp["CO"]["binned_spectrum"][()]
-co2_xsec = absorp["CO2"]["binned_spectrum"][()]
-h2o_xsec = absorp["H2O"]["binned_spectrum"][()]
-
-plt.figure(1)
-plt.plot(binned_spectrum, co_xsec)
-plt.plot(binned_spectrum, co2_xsec)
-plt.plot(binned_spectrum, h2o_xsec)
-
-plt.savefig(f"{PLOT_SAVE_DIR}/binned_contrib.png", dpi=300)
+DATA_DIR = "DATA"
 
 
-# Native spectrum
-native_spectrum = spectrum["native_wlgrid"][()]
-absorp = sol0["Spectra"]["Contributions"]["Absorption"]
-co_xsec = absorp["CO"]["native_spectrum"][()]
-co2_xsec = absorp["CO2"]["native_spectrum"][()]
-h2o_xsec = absorp["H2O"]["native_spectrum"][()]
+def main():
+    nocloud = dar.create_taurex_solution(
+        filename=f"{DATA_DIR}/nocloud.hdf5",
+        name_id="No Clouds"
+    )
 
-plt.figure(2)
-plt.plot(native_spectrum, co_xsec)
-plt.plot(native_spectrum, co2_xsec)
-plt.plot(native_spectrum, h2o_xsec)
-plt.xlim(3, 5)
+    simcloud = dar.create_taurex_solution(
+        filename=f"{DATA_DIR}/simplecloud.hdf5",
+        name_id="Simple Clouds"
+    )
 
-plt.savefig(f"{PLOT_SAVE_DIR}/native_contrib.png", dpi=300)
+    sols = [nocloud, simcloud]
+    fig, ax = plt.subplots(figsize=(8, 4))
+
+    plt.plot()
+
+    for entry in sols:
+        pg.retr_spec(entry, ax)
+
+    plt.legend()
+    plt.show()
 
 
-plt.show()
-
+if __name__ == "__main__":
+    pg.rc_setup()
+    main()
